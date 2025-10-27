ae25a8e1b01b1b8c3b50731728ad043562e5685a
raviwhitehat <ekravi26@gmail.com>
2025-10-27 23:24:36 +0530

    Learning about how to read input as bytes and then change them into char

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self,Read};
+
 fn main() {
-    println!("Hello, world!");
+
+    for b in io::stdin().bytes(){
+        println!("{:?}",b);
+        let c = b.unwrap() as char;
+
+        println!("{}",c)
+    }
+    
 }
