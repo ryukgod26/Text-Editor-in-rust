3c3398054b1cee0b06d6656d24f83bd0b0b89914
raviwhitehat <ekravi26@gmail.com>
2025-10-27 22:56:16 +0530

    Craeted First program with cargo in rust

---
+++ hecto/target/debug/deps/hecto.d
@@ -0,0 +1,5 @@
+D:\Text_Editor_in_rust\Text-Editor-in-rust\hecto\target\debug\deps\hecto.d: src\main.rs
+
+D:\Text_Editor_in_rust\Text-Editor-in-rust\hecto\target\debug\deps\hecto.exe: src\main.rs
+
+src\main.rs:
---
+++ hecto/target/debug/hecto.d
@@ -0,0 +1,1 @@
+D:\Text_Editor_in_rust\Text-Editor-in-rust\hecto\target\debug\hecto.exe: D:\Text_Editor_in_rust\Text-Editor-in-rust\hecto\src\main.rs
