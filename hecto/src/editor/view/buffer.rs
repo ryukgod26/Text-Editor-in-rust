d672a58d1ce9cb4731e011b9456dac686d1fe034
ryukgod26 <ekravi26@gmail.com>
2025-11-22 07:35:39 +0000

    Created a Simple Status Bar whixh show that if the fiel is modified or not

--- hecto/src/editor/view/buffer.rs
+++ hecto/src/editor/view/buffer.rs
@@ -6,7 +6,8 @@ use st::io::Write;
 #[derive(Default)]
 pub struct Buffer{
     pub lines :Vec<Line>,
-    filename: Option<String>,
+    pub filename: Option<String>,
+    pub dirty: bool,
 }
 
 impl Buffer{
@@ -24,6 +25,7 @@ for line in contents.lines(){
 Ok(Self{
     lines,
     Some(filename.to_string()),
+    dirty: false,
     })
 }
 
@@ -35,11 +37,13 @@ pub fn insert_char(&mut self,character: char,at: Location){
     if at.line_index > self.height() {
             return;
         }
-    else if at.line_index == self.lines.len() {
+    else if at.line_index == self.height() {
         self.lines.push(Line::from(&character.to_string()));
+        self.dirty = true;
         }
     else if let Some(line) = self.lines.get_mut(at.line_index){
             line.insert_char(character,at.grapheme_index);
+            self.dirty = true;
         }
     }
 
@@ -50,29 +54,34 @@ pub fn delete(&mut self,at: Location){
 
                 #[allow(clippy::index_slicing)]
                 self.lines[at.line_index].append(&next_line);
+                self.dirty = true;
             } else if at.grapheme_index < line.grapheme_count() {
                 #[allow(clippy::index_slicing)]
                 self.lines[at.line_index].remove(at.grapheme_index);
+                self.dirty = true;
             }
         }
     }
 
 pub fn insert_newline(&mut self,at: Location){
-    if at.line_index == self.heifht() {
+    if at.line_index == self.height() {
         self.push(Line::default());
+        self.dirty = true;
         } else if let Some(line) = self.lines.get_mut(at.line_index){
         let new = line.split(at.grapheme_index);
         self.lines.insert(at.line_index.saturating_add(1),new);
+        self.dirty = true;
         }
 
     }
 
-pub fn save(&self) -> Result<(),std::io::Error>{
+pub fn save(&mut self) -> Result<(),std::io::Error>{
     if let Some(filename) = &self.filename{
         let mut File = File::create(filename)?;
         for line in &self.lines{
                 writeln!(file,"{line}")?;
             }
+        self.dirty = false;
         }
     Ok(())
     }
