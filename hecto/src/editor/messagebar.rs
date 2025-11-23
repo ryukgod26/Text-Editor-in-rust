68724d8b397b3dfa02bfe41553f6020dc6e52d1f
ryukgod26 <ekravi26@gmail.com>
2025-11-23 19:09:14 +0000

    Added a Expiry Timer to the messages after which messages disappear

--- hecto/src/editor/messagebar.rs
+++ hecto/src/editor/messagebar.rs
@@ -2,19 +2,30 @@ use super ::{
     termibal::{Size,Terminal},
     uicomponent::UIComponent,
 };
+use std::time::{Instant,Duration};
+
+const DEFAULT_DISAPPEAR_DURATION: Duration = Duration::new(5,0);
+
+struct Message{
+    text: String,
+    time: Instant,
+}
 
 #[derive(Default)]
 pub struct MessageBar{
-    curremt_message: String,
+    current_message: Message,
     needs_redraw: bool,
+    cleared_after_expiry: bool,
 }
 
 impl MessageBar{
     pub fn update_message(&mut self, new_message: String){
-        if new_message != self.current_message{
-            self.current_message = new_messagw;
-            self.mark_redraw(true);
-        }
+        self.message = Message{
+            text: new_message,
+            time: Instant::now(),
+        };
+        self.cleared_after_enquiry = false;
+        self.mark_redraw(true);
     }
 }
 
@@ -31,5 +42,20 @@ impl UIComponent for MessageBar{
 
     fn draw(&mut self, origin: usize) -> Result<(),std::io::Error>{
         Terminal::print_row(origin,&self.current_message)
+    }
+}
+
+impl Message{
+    fn is_expired(&self) -> bool{
+        Instant::now().duration_since(self.time) > DEFAULT_DISAPPEAR_DURATION
+    }
+}
+
+impl Default for Message{
+    fn default() -> Self{
+        Self{
+            text: String::new(),
+            time: Instant::now(),
+        }
     }
 }
