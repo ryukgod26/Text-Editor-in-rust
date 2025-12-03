e877a242d88324a1f214fc463bf42a85cc419c2f
ryukgod26 <ekravi26@gmail.com>
2025-12-03 03:55:25 +0000

    Added Highlighting for Hexdecimal, Octahedral and Binary Numbers.

--- hecto/src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
+++ hecto/src/editor/uicomponents/view/highlighter/rustsyntaxhighlighter.rs
@@ -8,11 +8,32 @@ pub struct RustSyntaxHighlighter{
     highlights: HashMap<LineIdx, Vec<Annotation>>,
 }
 
-impl RustSyntaxHighlighter{
+
+
+impl SyntaxHighlighter for RustSyntaxHighlighter{
+    fn highlight(&mut self, line_idx: LineIdx, line: &Line){
+        let mut result = Vec::new();
+        for(start_idx, word) in line.split_word_bound_indices(){
+            if is_valid_digit(word) {
+                result.push(Annotation{
+                    annotation_type: AnnotationType::Digit,
+                    start: start_idx,
+                    end: start_idx.saturating_add(word.len()),
+                });
+            }
+        }
+        self.highlights.insert(line_idx, result);
+    }
+
     fn is_valid_digit(word: &str)  -> bool{
         if word.is_empty() {
             return false;
         }
+
+        if is_numerical_lateral(word){
+            return true;
+        }
+
         let mut chars = word.chars();
         
         if let Some(first_char) = chars.next() && !first_char.is_ascii_digit(){
@@ -49,21 +70,19 @@ impl RustSyntaxHighlighter{
         }
         prev_digit    
     }
-}
 
-impl SyntaxHighlighter for RustSyntaxHighlighter{
-    fn highlight(&mut self, line_idx: LineIdx, line: &Line){
-        let mut result = Vec::new();
-        for(start_idx, word) in line.split_word_bound_indices(){
-            if is_valid_digit(word) {
-                result.push(Annotation{
-                    annotation_type: AnnotationType::Digit,
-                    start: start_idx,
-                    end: start_idx.saturating_add(word.len()),
-                });
-            }
+    fn is_numeric_literal(word: &str) -> bool{
+        if word.len() <= 2 {
+            return false;
         }
-        self.highlights.insert(line_idx, result);
+        let base = chars.next(){
+            Some('b' | 'B') => 2,
+            Some('o' | 'O') => 8,
+            Some('x' | 'X') => 16,
+            _ => return false;
+        };
+        chars.all(|char| char.is_digit(base))
+
     }
 
     fn get_annotations(&self, line_idx: LineIdx) -> Option<&Vex<Annotation>>{
